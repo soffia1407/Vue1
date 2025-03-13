f8ac411f095edb1dee3ed1f3fa9859133f0d1478
soffia1407 <sff140705@gmail.com>
2025-03-13 23:18:31 +0700

    added list rendering

--- js/main.js
+++ js/main.js
@@ -8,7 +8,19 @@ let app = new Vue({
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         inStock: true,
         inventory: 100,
-        onSale: true
+        onSale: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+         ],
+        sizes: ['34-35', '36-37', '38-39', '40-41', '42-43', '44-45']
     }
  })
  
