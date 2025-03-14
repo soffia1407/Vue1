73d5c6e61b2c1941773ef139ba7a73fc5a2c6665
soffia1407 <sff140705@gmail.com>
2025-03-14 19:11:00 +0700

    the rows were joined, and the image property was replaced with selectedVariant

--- js/main.js
+++ js/main.js
@@ -2,29 +2,29 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
+        brand: 'Vue Mastery',
         description: "A pair of warm, fuzzy socks.",
-        image: "./assets/vmSocks-green-onWhite.jpg",
+        selectedVariant: 0,
         altText: "A pair of socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         inStock: true,
         inventory: 100,
-        onSale: true,
+        onSale: false,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0
             }
-         ],
-        updateProduct(variantImage) {
-           this.image = variantImage
-        },                  
+         ],                           
         sizes: ['34-35', '36-37', '38-39', '40-41', '42-43', '44-45'],
         cart: 0,
     },
@@ -34,6 +34,33 @@ let app = new Vue({
         },
         deleteFromCart(){
             this.cart -= 1
-        }
+        },
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
+        }         
     },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity /*Если теперь навести указатель мыши на квадрат, 
+            количество товара, соответствующее которому, равняется нулю, в inStock попадёт 0, 
+            а 0 является в JavaScript значением, приводимым к логическому значению false. 
+            Из-за этого на странице будет выведено сообщение Out of Stock.
+            почему-то надпись не меняется
+            */
+        },
+        sale() { // Добавляем вычисляемое свойство sale
+            if (this.onSale) {
+                return this.brand + ' ' + this.product + ' is on sale!';
+            } else {
+                return this.brand + ' ' + this.product + ' is not on sale';
+            }
+        }
+     }     
 })
