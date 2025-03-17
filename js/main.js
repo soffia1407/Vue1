8565cae67d1639d631527bc50dcd3440c59c40e0
soffia1407 <sff140705@gmail.com>
2025-03-17 18:25:24 +0700

    I've removed the code for practical work

--- js/main.js
+++ js/main.js
@@ -3,13 +3,8 @@ let app = new Vue({
     data: {
         product: "Socks",
         brand: 'Vue Mastery',
-        description: "A pair of warm, fuzzy socks.",
         selectedVariant: 0,
         altText: "A pair of socks",
-        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-        inStock: true,
-        inventory: 100,
-        onSale: false,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
@@ -24,21 +19,17 @@ let app = new Vue({
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
                 variantQuantity: 0
             }
-         ],                           
-        sizes: ['34-35', '36-37', '38-39', '40-41', '42-43', '44-45'],
-        cart: 0,
+        ],
+        cart: 0
     },
     methods: {
         addToCart() {
             this.cart += 1
         },
-        deleteFromCart(){
-            this.cart -= 1
-        },
         updateProduct(index) {
             this.selectedVariant = index;
             console.log(index);
-        }         
+        }
     },
     computed: {
         title() {
@@ -48,19 +39,8 @@ let app = new Vue({
             return this.variants[this.selectedVariant].variantImage;
         },
         inStock(){
-            return this.variants[this.selectedVariant].variantQuantity /*Если теперь навести указатель мыши на квадрат, 
-            количество товара, соответствующее которому, равняется нулю, в inStock попадёт 0, 
-            а 0 является в JavaScript значением, приводимым к логическому значению false. 
-            Из-за этого на странице будет выведено сообщение Out of Stock.
-            почему-то надпись не меняется
-            */
-        },
-        sale() { // Добавляем вычисляемое свойство sale
-            if (this.onSale) {
-                return this.brand + ' ' + this.product + ' is on sale!';
-            } else {
-                return this.brand + ' ' + this.product + ' is not on sale';
-            }
+            return this.variants[this.selectedVariant].variantQuantity
         }
-     }     
-})
+    }
+ })
+ 
